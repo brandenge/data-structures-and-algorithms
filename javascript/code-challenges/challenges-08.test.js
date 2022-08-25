9f18b8ef84fec72a0ea91698e9f1af2f9bab39bd
Branden Ge <86636108+brandenge@users.noreply.github.com>
2022-08-25 11:39:11 -0500

    Improved challenge 6 solution slightly

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -110,7 +110,7 @@ CHALLENGE 6
 Write a function named citiesAtoJ that takes in an array of city names and uses a regular expression pattern to return a new array containing any cities that begin with the letters A through J, inclusive.
 ------------------------------------------------------------------------------------------------ */
 
-const citiesAtoJ = (arr) => arr.filter(city => /[A-J]/.test(city[0]));
+const citiesAtoJ = (arr) => arr.filter(city => /^[A-J]/.test(city[0]));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
