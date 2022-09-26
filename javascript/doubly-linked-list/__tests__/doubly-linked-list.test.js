10634de625cc7bb09b7e38d136f6588cbc3f399c
Branden Ge <86636108+brandenge@users.noreply.github.com>
2022-09-26 01:00:32 -0500

    Fixed errors with not using the correct name for DoublyLinkedList

--- javascript/doubly-linked-list/__tests__/doubly-linked-list.test.js
+++ javascript/doubly-linked-list/__tests__/doubly-linked-list.test.js
@@ -6,77 +6,77 @@ const DoublyLinkedList = require('../index');
 describe('Doubly Linked List', () => {
 
   it('Instantiates an empty linked list', () => {
-    const linkedList = new DoublyLinkedList();
-    expect(linkedList.head).toEqual(null);
+    const doublyLinkedList = new DoublyLinkedList();
+    expect(doublyLinkedList.head).toEqual(null);
   });
 
   it('Inserts a new node at the head of the linked list.', () => {
-    const linkedList = new DoublyLinkedList();
-    linkedList.insert(1);
-    expect(linkedList.head.value).toEqual(1);
+    const doublyLinkedList = new DoublyLinkedList();
+    doublyLinkedList.insert(1);
+    expect(doublyLinkedList.head.value).toEqual(1);
   });
 
   it('The head property always points to the first node of the linked list.', () => {
-    const linkedList = new DoublyLinkedList();
-    linkedList.insert(1);
-    linkedList.insert(2);
-    expect(linkedList.head.value).toEqual(2);
+    const doublyLinkedList = new DoublyLinkedList();
+    doublyLinkedList.insert(1);
+    doublyLinkedList.insert(2);
+    expect(doublyLinkedList.head.value).toEqual(2);
   });
 
   it('Can insert multiple nodes into the linked list.', () => {
-    const linkedList = new DoublyLinkedList();
-    linkedList.insert(1);
-    linkedList.insert(2);
-    linkedList.insert(3);
-    expect(linkedList.head.value).toEqual(3);
-    expect(linkedList.head.next.value).toEqual(2);
-    expect(linkedList.head.next.next.value).toEqual(1);
+    const doublyLinkedList = new DoublyLinkedList();
+    doublyLinkedList.insert(1);
+    doublyLinkedList.insert(2);
+    doublyLinkedList.insert(3);
+    expect(doublyLinkedList.head.value).toEqual(3);
+    expect(doublyLinkedList.head.next.value).toEqual(2);
+    expect(doublyLinkedList.head.next.next.value).toEqual(1);
   });
 
   it('It can traverse in reverse through the doubly linked list.', () => {
-    const linkedList = new DoublyLinkedList();
-    linkedList.insert(1);
-    linkedList.insert(2);
-    linkedList.insert(3);
-    expect(linkedList.head.next.next.prev.prev.value).toEqual(3);
+    const doublyLinkedList = new DoublyLinkedList();
+    doublyLinkedList.insert(1);
+    doublyLinkedList.insert(2);
+    doublyLinkedList.insert(3);
+    expect(doublyLinkedList.head.next.next.prev.prev.value).toEqual(3);
   });
 
   it('It will not create a circular linked list.', () => {
-    const linkedList = new DoublyLinkedList();
-    linkedList.insert(1);
-    expect(linkedList.head.prev).toEqual(null);
-    expect(linkedList.head.next).toEqual(null);
-    linkedList.insert(2);
-    expect(linkedList.head.prev).toEqual(null);
-    expect(linkedList.head.next.next).toEqual(null);
-    linkedList.insert(3);
-    expect(linkedList.head.prev).toEqual(null);
-    expect(linkedList.head.next.next.next).toEqual(null);
+    const doublyLinkedList = new DoublyLinkedList();
+    doublyLinkedList.insert(1);
+    expect(doublyLinkedList.head.prev).toEqual(null);
+    expect(doublyLinkedList.head.next).toEqual(null);
+    doublyLinkedList.insert(2);
+    expect(doublyLinkedList.head.prev).toEqual(null);
+    expect(doublyLinkedList.head.next.next).toEqual(null);
+    doublyLinkedList.insert(3);
+    expect(doublyLinkedList.head.prev).toEqual(null);
+    expect(doublyLinkedList.head.next.next.next).toEqual(null);
   });
 
   it('Will return true when finding a value within the linked list that exists.', () => {
-    const linkedList = new DoublyLinkedList();
-    linkedList.insert(1);
-    linkedList.insert(2);
-    linkedList.insert(3);
-    expect(linkedList.includes(1)).toEqual(true);
-    expect(linkedList.includes(2)).toEqual(true);
-    expect(linkedList.includes(3)).toEqual(true);
+    const doublyLinkedList = new DoublyLinkedList();
+    doublyLinkedList.insert(1);
+    doublyLinkedList.insert(2);
+    doublyLinkedList.insert(3);
+    expect(doublyLinkedList.includes(1)).toEqual(true);
+    expect(doublyLinkedList.includes(2)).toEqual(true);
+    expect(doublyLinkedList.includes(3)).toEqual(true);
   });
 
   it('Will return false when searching for a value in the linked list that does not exist.', () => {
-    const linkedList = new DoublyLinkedList();
-    linkedList.insert(1);
-    linkedList.insert(2);
-    linkedList.insert(3);
-    expect(linkedList.includes(4)).toEqual(false);
+    const doublyLinkedList = new DoublyLinkedList();
+    doublyLinkedList.insert(1);
+    doublyLinkedList.insert(2);
+    doublyLinkedList.insert(3);
+    expect(doublyLinkedList.includes(4)).toEqual(false);
   });
 
   it('Can properly return a collection of all the values that exist in the linked list (returned as a string).', () => {
-    const linkedList = new DoublyLinkedList();
-    linkedList.insert(1);
-    linkedList.insert(2);
-    linkedList.insert(3);
-    expect(linkedList.toString()).toEqual(`NULL <- { 3 } ->  <- { 2 } ->  <- { 1 } -> NULL`);
+    const doublyLinkedList = new DoublyLinkedList();
+    doublyLinkedList.insert(1);
+    doublyLinkedList.insert(2);
+    doublyLinkedList.insert(3);
+    expect(doublyLinkedList.toString()).toEqual(`NULL <- { 3 } ->  <- { 2 } ->  <- { 1 } -> NULL`);
   });
 });
