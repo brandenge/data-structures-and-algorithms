ed7b5feef5106d72b8d01a50a19f36f6af0022c8
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-10-16 15:24:24 -0500

    Fix: tests for binary search trees to add coverage for missed lines

--- python/fixtures/binary_search_trees_with_iteration.py
+++ python/fixtures/binary_search_trees_with_iteration.py
@@ -8,19 +8,19 @@ def tree():
 @pytest.fixture
 def small_tree():
     tree = Tree()
-    tree.add(1)
-    tree.add(2)
-    tree.add(3)
+    tree.add(0)
+    tree.add(-10)
+    tree.add(10)
     return tree
 
 @pytest.fixture
 def medium_tree():
     tree = Tree()
-    tree.add(1)
-    tree.add(2)
-    tree.add(3)
-    tree.add(4)
+    tree.add(0)
+    tree.add(-10)
+    tree.add(10)
+    tree.add(-5)
     tree.add(5)
-    tree.add(6)
-    tree.add(7)
+    tree.add(-15)
+    tree.add(15)
     return tree
--- python/tests/data_structures/test_binary_search_tree_with_iteration.py
+++ python/tests/data_structures/test_binary_search_tree_with_iteration.py
@@ -10,26 +10,62 @@ def test_initialization(tree):
 
 def test_add(tree):
     assert tree.breadth_first_traversal() == []
-    assert tree.add(1) == 1
-    assert tree.breadth_first_traversal() == [1]
-    assert tree.add(2) == 2
-    assert tree.breadth_first_traversal() == [1, 2]
-    assert tree.add(3) == 3
-    assert tree.breadth_first_traversal() == [1, 2, 3]
-    assert tree.add(4) == 4
-    assert tree.breadth_first_traversal() == [1, 2, 3, 4]
+    assert tree.add(0) == 0
+    assert tree.breadth_first_traversal() == [0]
+    assert tree.add(-10) == -10
+    assert tree.breadth_first_traversal() == [0, -10]
+    assert tree.add(10) == 10
+    assert tree.breadth_first_traversal() == [0, -10, 10]
+    assert tree.add(-20) == -20
+    assert tree.breadth_first_traversal() == [0, -10, 10, -20]
+    assert tree.add(20) == 20
+    assert tree.breadth_first_traversal() == [0, -10, 10, -20, 20]
+    assert tree.add(-5) == -5
+    assert tree.breadth_first_traversal() == [0, -10, 10, -20, -5, 20]
     assert tree.add(5) == 5
-    assert tree.breadth_first_traversal() == [1, 2, 3, 4, 5]
-    assert tree.add(6) == 6
-    assert tree.breadth_first_traversal() == [1, 2, 3, 4, 5, 6]
-    assert tree.add(7) == 7
-    assert tree.breadth_first_traversal() == [1, 2, 3, 4, 5, 6, 7]
+    assert tree.breadth_first_traversal() == [0, -10, 10, -20, -5, 5, 20]
+    assert tree.add(15) == 15
+    assert tree.breadth_first_traversal() == [0, -10, 10, -20, -5, 5, 20, 15]
+    assert tree.add(-15) == -15
+    assert tree.breadth_first_traversal() == [0, -10, 10, -20, -5, 5, 20, -15, 15]
 
 def test_includes(tree, medium_tree):
-    assert tree.includes(1) is False
-    assert medium_tree.includes(4) is True
-    assert medium_tree.includes(5) is True
-    assert medium_tree.includes(6) is True
-    assert medium_tree.includes(7) is True
-    assert medium_tree.includes(0) is False
-    assert medium_tree.includes(10) is False
+    assert tree.includes(0) is False
+    assert medium_tree.includes(0) is True
+    assert medium_tree.includes(-10) is True
+    assert medium_tree.includes(10) is True
+    assert medium_tree.includes(-5) is True
+    assert medium_tree.includes(-15) is True
+    assert medium_tree.includes(15) is True
+    assert medium_tree.includes(-1) is False
+    assert medium_tree.includes(82) is False
+
+def test_breadth_first_traversal(tree, small_tree, medium_tree):
+    assert tree.breadth_first_traversal() == []
+    assert small_tree.breadth_first_traversal() == [0, -10, 10]
+    assert medium_tree.breadth_first_traversal() == [0, -10, 10, -15, -5, 5, 15]
+
+def test_dfs_pre_order_alternative(tree, small_tree, medium_tree):
+    assert tree.dfs_pre_order_alternative() == []
+    assert small_tree.dfs_pre_order_alternative() == [0, -10, 10]
+    assert medium_tree.dfs_pre_order_alternative() == [0, -10, -15, -5, 10, 5, 15]
+
+def test_dfs_pre_order(tree, small_tree, medium_tree):
+    assert tree.dfs_pre_order() == []
+    assert small_tree.dfs_pre_order() == [0, -10, 10]
+    assert medium_tree.dfs_pre_order() == [0, -10, -15, -5, 10, 5, 15]
+
+def test_dfs_in_order(tree, small_tree, medium_tree):
+    assert tree.dfs_in_order() == []
+    assert small_tree.dfs_in_order() == [-10, 0, 10]
+    assert medium_tree.dfs_in_order() == [-15, -10, -5, 0, 5, 10, 15]
+
+def test_dfs_post_order(tree, small_tree, medium_tree):
+    assert tree.dfs_post_order() == []
+    assert small_tree.dfs_post_order() == [-10, 10, 0]
+    assert medium_tree.dfs_post_order() == [-15, -5, -10, 5, 15, 10, 0]
+
+def test_dfs_post_order_two_stacks(tree, small_tree, medium_tree):
+    assert tree.dfs_post_order_two_stacks() == []
+    assert small_tree.dfs_post_order_two_stacks() == [-10, 10, 0]
+    assert medium_tree.dfs_post_order_two_stacks() == [-15, -5, -10, 5, 15, 10, 0]
